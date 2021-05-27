b9a50b7603d91db13d12902a628fe148481a9083
An Pham <anpham@cttdvnatl.org>
2021-05-27 15:50:50 -0400

    packages installation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,14 @@ import React from 'react';
 const Header = () => {
     return (
         <>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Events</li>
+                    <li>Food Menu</li>
+                    <li>About</li>
+                </ul>
+            </nav>
         </>
     )
 }
