6b25828c8348c9fe8970a64068f65094473d515b
An Pham <anpham@cttdvnatl.org>
2021-05-28 18:05:39 -0400

    Multi-lang changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,46 @@ import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom';
+import {setLanguage, getLanguage, setTranslations, setDefaultLanguage} from 'react-multi-lang';
+import en from './database/en.json';
+import vn from './database/vn.json';
 import HomePage from './pages/HomePage';
+
+//Website Cookie for Language
+setTranslations({vn, en})
+setDefaultLanguage('en')
+
+function checkLangCookie() {
+    const language = getCookie("language");
+    if (language === 'en') {
+        setLanguage('en');
+    } else if (language === 'vn') {
+        setLanguage('vn');
+    } else {
+        setLangCookie();
+    }
+}
+
+function setLangCookie() {
+    document.cookie = "language = " + getLanguage();
+}
+
+function getCookie(cookieParam) {
+    const cookieName = cookieParam + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookieArray = decodedCookie.split(';');
+    for (let i = 0; i < cookieArray.length; i++) {
+        let c = cookieArray[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(cookieName) === 0) {
+            return c.substring(cookieName.length, c.length);
+        }
+    }
+}
+
+checkLangCookie()
 
 function App() {
   return (
