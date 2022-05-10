f4a0213aa453c0192d6c611b48c8b40ba33c0555
An Pham <anpham@cttdvnatl.org>
2022-05-10 11:02:45 -0400

    contact page css

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -38,37 +38,39 @@ const ContactPage = () => {
 
     return (
         <>
-        <div class="contact-page">
-            <h1>{t('contact.pageTitle')}</h1>
-            <form ref={form} onSubmit={sendEmail}>
-                {/* False Form/Honeypot */}
-                <input type="text" name="_form" style={{display: 'none'}}/>
-                <div class="contact-form-row">
-                    <div class="contact-form-input contact-form-input-name">
-                        <input placeholder={' ' + t('contact.textBoxName')} ref={nameInput} type="text" name="user_name" />
+        <div class="contact-page">   
+            <div class="contact-form">
+                <h1>{t('contact.pageTitle')}</h1>
+                <form ref={form} onSubmit={sendEmail}>
+                    {/* False Form/Honeypot */}
+                    <input type="text" name="_form" style={{display: 'none'}}/>
+                    <div class="contact-form-row">
+                        <div class="contact-form-input contact-form-input-name">
+                            <input placeholder={' ' + t('contact.textBoxName')} ref={nameInput} type="text" name="user_name" />
+                        </div>
+                        <div class="contact-form-input contact-form-input-email">
+                            <input placeholder={' ' + t('contact.textBoxEmail')} ref={emailInput} type="email" name="user_email" />
+                        </div>
                     </div>
-                    <div class="contact-form-input contact-form-input-email">
-                        <input placeholder={' ' + t('contact.textBoxEmail')} ref={emailInput} type="email" name="user_email" />
+                    <div class="contact-form-row">
+                        <div class="contact-form-input contact-form-input-message">
+                            <textarea placeholder={' ' + t('contact.textBoxMessage')} ref={message} name="message" />
+                        </div>
                     </div>
-                </div>
-                <div class="contact-form-row">
-                    <div class="contact-form-input contact-form-input-message">
-                        <textarea placeholder={' ' + t('contact.textBoxMessage')} ref={message} name="message" />
+                    <h4 ref={textErrorMessage} style={{padding: '10px', margin: 'auto', fontFamily: 'sans-serif', display: 'none', color: 'red'}}>Fill in ALL fields</h4>
+                    <h4 ref={captchaErrorMessage} style={{padding: '10px', margin: 'auto', fontFamily: 'sans-serif', display: 'none', color: 'red'}}>Captcha Failed</h4>
+                    <div class="captcha" style={{textAlign: 'center'}}>
+                            <ReCAPTCHA
+                                style={{margin: 'auto', display: 'inline-block'}}
+                                sitekey="6LdxPhkeAAAAAKQ0myRsb9eAPskcHKCn8rYNBncj"
+                                onChange={() => setCaptcha(true)}
+                            />
                     </div>
-                </div>
-                <h4 ref={textErrorMessage} style={{padding: '10px', margin: 'auto', fontFamily: 'sans-serif', display: 'none', color: 'red'}}>Fill in ALL fields</h4>
-                <h4 ref={captchaErrorMessage} style={{padding: '10px', margin: 'auto', fontFamily: 'sans-serif', display: 'none', color: 'red'}}>Captcha Failed</h4>
-                <div class="captcha" style={{textAlign: 'center'}}>
-                        <ReCAPTCHA
-                            style={{margin: 'auto', display: 'inline-block'}}
-                            sitekey="6LdxPhkeAAAAAKQ0myRsb9eAPskcHKCn8rYNBncj"
-                            onChange={() => setCaptcha(true)}
-                        />
-                </div>
-                <div class="contact-form-row">
-                    <input ref={submit} class="contact-form-submit" type="submit" value="Send" />
-                </div>
-            </form>
+                    <div class="contact-form-row">
+                        <input ref={submit} class="contact-form-submit" type="submit" value="Send" />
+                    </div>
+                </form>
+            </div>
         </div>
         </>
     )
