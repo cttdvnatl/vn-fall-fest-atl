d1ecc2638c90fa1a862c9c69952f1d934e176829
An Pham <anpham@cttdvnatl.org>
2022-05-13 23:14:28 -0400

    revamped carousel with transition added

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -16,22 +16,42 @@ const CarouselList = ({list}) => {
     }
 */
 
-    setTimeout(() => {
-        setImage(currentImage === length - 1 ? 0 : currentImage + 1);
-    }, 3000)
+    const carouselTimeout =
+        setTimeout(() => {
+            setImage(currentImage === length - 1 ? 0 : currentImage + 1);
+        }, 3000)
+    
+    if (carouselTimeout === 1) { //To prevent React alert for not using variables
+        return `1`;
+    }
 
     return (
         <>
         {list.map((carousel, index) => (
             <div class={index === currentImage ? "carousel active" : "carousel"} key={carousel.title} aria-hidden={index !== currentImage} style={{}}>
         {/*
-                    <div class="carousel-overlay-left" onClick={prevSlide}></div>
-                    <div class="carousel-overlay-right" onClick={nextSlide}></div>
+                <div class="carousel-overlay-left" onClick={prevSlide}></div>
+                <div class="carousel-overlay-right" onClick={nextSlide}></div>
         */}
-                    <img alt={carousel.imageNumber} src={carousel.image}></img>
-                    <div class="filler"/>
+                <img alt={carousel.imageNumber} src={carousel.image}></img>
             </div>
         ))}
+        <img alt="filler" class="filler" src="http://www.hvmatl.net/gallery/LHMT_img/LHMT2022/Carousel/LHMT2022Invite.png"/>
+        {/*<div>
+            <li onClick={ () => {
+                    clearTimeout(carouselTimeout);
+                    setImage(0);
+                    clearTimeout(carouselTimeout);
+                }
+            }
+            ></li>
+            <li onClick={ () => {
+                    clearTimeout(carouselTimeout);
+                    setImage(1);
+                    clearTimeout(carouselTimeout);
+                }
+            }
+            ></li></div>*/}
         </>
     )
 }
@@ -52,7 +72,7 @@ const Carousel = () => {
     return (
         <>
         <div class="carousel-container">
-            <CarouselList list={Data} />
+            <CarouselList list={Data}/>
         </div>
         </>
     )
