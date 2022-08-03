9fc34b734b687eba776bfc0262c92dfca47007ac
benjaminluan <benjaminluan1@gmail.com>
2022-08-03 18:38:16 -0400

    Added promo video and background picture

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Landing = () => {
+  let [video, setVideo] = useState(true);
+
   return (
     <div className="landing">
       <div className="background__wrapper">
         {/* <video preload autoPlay muted className="background__video">
           <source src="/resources/pages/home/Promo.mp4" type="video/mp4" />
         </video> */}
+        <div className={"video" + (video === true ? " active" : "")}>
+          <span
+            onClick={() => {
+              setVideo(false);
+            }}
+          >
+            X
+          </span>
+          <video width="750" height="500" preload autoPlay muted>
+            <source src="/resources/pages/home/Promo.mp4" type="video/mp4" />
+          </video>
+          <div className="video__overlay" />
+        </div>
+
         <a
           href="https://hvmatl.com/gallery/LHMT_img/LHMT2022/Carousel/MainPoster.jpg"
           target={"_blank"}
@@ -29,57 +45,58 @@ const Landing = () => {
           </p>
         </div>
       </div>
+
       <div className="rewards">
-        <div className="share__container">
-          <div className="share">
-            <h3 className="share__header">Share!</h3>
-            <p className="share__para">
+        <div className="reward__container">
+          <div className="reward">
+            <h3 className="reward__header">Share!</h3>
+            <p className="reward__para">
               Come join us in liking your favorite singers and sharing the post!
             </p>
-            <p className="share__para">
+            <p className="reward__para">
               We will be choosing 10 random individuals that "shared or liked"
               before August 7, 2022 to recieve a limited edition stainless stell
               mug with the 2022 Fall Festival Logo!
             </p>
           </div>
-          <figure className="share__image--wrapper">
+          <figure className="reward__image--wrapper">
             <img
               src="http://www.hvmatl.net/gallery/LHMT_img/LHMT2022/Carousel/MugGift.jpg"
               alt=""
-              className="share__image"
+              className="reward__image"
             />
           </figure>
         </div>
-        <div className="raffle__container">
-          <figure className="raffle__image--wrapper">
+        <div className="reward__container">
+          <figure className="reward__image--wrapper">
             <img
               src="https://hvmatl.com/gallery/LHMT_img/LHMT2022/Pages/Raffles/VesoPromo.png"
               alt=""
-              className="raffle__image"
+              className="reward__image"
             />
           </figure>
-          <div className="raffle">
-            <h3 className="raffle__header">Raffle!</h3>
-            <p className="raffle__para">10 chances to WIN 10 great prizes</p>
-            <p className="raffle__para">
+          <div className="reward">
+            <h3 className="reward__header">Raffle!</h3>
+            <p className="reward__para">10 chances to WIN 10 great prizes</p>
+            <p className="reward__para">
               Grab 5 raffles and get 1 FREE! Contact us at
               fallfest.raffle@hvmatl.org to purchase your raffle tickets.
             </p>
           </div>
         </div>
-        <div className="prize__container">
-          <div className="prize">
-            <h3 className="prize__header">Grand Prize!</h3>
-            <p className="prize__para">
+        <div className="reward__container">
+          <div className="reward">
+            <h3 className="reward__header">Grand Prize!</h3>
+            <p className="reward__para">
               You can be the luckiest of them all on September 4th and bring
               yourself a BRAND NEW LEXUS!!!
             </p>
           </div>
-          <figure className="prize__image--wrapper">
+          <figure className="reward__image--wrapper">
             <img
               src="https://hvmatl.com/gallery/LHMT_img/LHMT2022/Pages/Raffles/Raffle.jpg"
               alt=""
-              className="prize__image"
+              className="reward__image"
             />
           </figure>
         </div>
