0c6ff5e6b5ff7cf2734bd47ec1b80da9de361570
josephmai19 <joeymai@hvmatl.gov>
2022-08-31 16:35:41 -0400

    Added new schedule

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -16,26 +16,19 @@ const Landing = () => {
         {/* <video preload autoPlay muted className="background__video">
           <source src="/resources/pages/home/Promo.mp4" type="video/mp4" />
         </video> */}
-        <div className={"video" + (video === true ? " active" : "")}>
-          <span
-            onClick={() => {
-              setVideo(false);
-            }}
-          >
-            X
-          </span>
-          <video width="750" height="500" preload autoPlay muted>
-            <source src="/resources/pages/home/Promo.mp4" type="video/mp4" />
-          </video>
-          <div className="video__overlay" />
-        </div>
-
+ 
         <a
-          href="https://hvmatl.com/gallery/LHMT_img/LHMT2022/Carousel/MainPoster.jpg"
-          target={"_blank"}
+         /* href="https://hvmatl.com/gallery/LHMT_img/LHMT2022/Carousel/MainPoster.jpg"
+          target={"_blank"}*/
         >
+ 
           <img
-            src="/resources/pages/home/MainPoster2.jpg"
+            src="https://hvmatl.com/gallery/LHMT_img/LHMT2022/Pages/FestivalSchedule/GeneralProgram.jpg"
+            alt=""
+            className="background__image"
+          />
+          <img
+            src="https://hvmatl.com/gallery/LHMT_img/LHMT2022/Pages/FestivalSchedule/SpiritualProgram.jpg"
             alt=""
             className="background__image"
           />
@@ -92,36 +85,7 @@ const Landing = () => {
                       />
                   </figure>
               </div>
-        <div className="reward__container">
-          <div className="reward">
-            <h3 className="reward__header">Spirit Day</h3>
-            <p className="reward__para">
-               {t('landing.reward_6')}
-            </p>
-          </div>
-          <figure className="reward__image--wrapper">
-            <img
-              src="http://www.hvmatl.net/gallery/LHMT_img/LHMT2022/Carousel/SpiritDay.jpg"
-              alt=""
-              className="reward__image"
-            />
-          </figure>
-        </div>
-              <div className="reward__container">
-                  <div className="reward">
-                      <h3 className="reward__header">Like and Share!</h3>
-                      <p className="reward__para">
-                          {t('landing.reward_1')}
-                      </p>
-                  </div>
-                  <figure className="reward__image--wrapper">
-                      <img
-                          src="http://www.hvmatl.net/gallery/LHMT_img/LHMT2022/Carousel/MugGift1.jpg"
-                          alt=""
-                          className="reward__image"
-                      />
-                  </figure>
-              </div>
+ 
       </div>
     </div>
   );
