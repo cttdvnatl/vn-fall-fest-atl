0184959cb2e1e544b5cd58e19e3449e031641a53
Trinity Pham <>
2024-03-11 20:30:52 -0400

    eureka

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
 
-const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(favicon(__dirname + '/build/favicon.ico'));
@@ -21,4 +20,42 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port);
+
+const Eureka = require('eureka-js-client').Eureka;
+const client = new Eureka({
+  instance: {
+    app: 'hvmatl-fall-festival-frontend',
+    hostName: 'localhost',
+    ipAddr: '127.0.0.1',
+    port: {
+      '$': 3000,
+      '@enabled': true,
+    },
+    vipAddress: 'hvmatl-fall-festival-frontend',
+    dataCenterInfo: {
+      '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+       name: 'MyOwn',
+    },
+  },
+  eureka: {
+    host: '18.188.96.133',
+    port: 8761,
+    servicePath: '/eureka/apps',
+  },
+});
+
+function connectToEureka() {              
+  client.logger.level('debug');  
+  client.start(function(error) {
+    console.log('########################################################');
+    console.log(error || 'Eureka registration complete');   
+  }); 
+}
+
+connectToEureka();
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Listening at http://%s:%s', host, port);
+});
